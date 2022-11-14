1c533673260672110588a6d4c86a21b4ee3aafa2
maximeflageole <maximeflageole@gmail.com>
2022-11-14 16:55:08 -0500

    Not much added, comments mostly

--- IdleHeroes/Assets/Scripts/HeroData.cs
+++ IdleHeroes/Assets/Scripts/HeroData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class HeroData : ScriptableObject
 {
     [field: SerializeField]
-    public int BaseHealth { get; protected set; } = 100;
+    public int BaseHealth { get; protected set; } = 100; //Health will represent how long a hero can stay on a mission!
     [field: SerializeField]
     public int BaseDamage { get; protected set; } = 10;
 }
